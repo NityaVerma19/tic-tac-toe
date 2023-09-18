983ac227ba081e7d03fed16fe8c961204db7bb0a
Nitya Verma <vrmnitya@gmail.com>
2023-09-18 20:50:42 +0530

    Made the GUI

--- APP.py
+++ APP.py
@@ -7,11 +7,18 @@ class Ttt:
         self.root.title('TIC TAC TOE')
         self.root.geometry("500x500")
         self.root.configure(bg = "black")
+
+        self.home()
+
         self.root.mainloop()
+
+
     def home(self):
-        heading = Label(self.root ,text = 'TIC TAC TOE', bg = "black" , fg = "white" )
+        heading = Label(self.root ,text = 'TIC TAC TOE', bg = "black" , fg = "#C4006E" )
         heading.pack(pady = (20,20))
         heading.configure(font = ("Times New Roman" ,32 , 'bold') )
+
+
 
 ttt = Ttt()
 
